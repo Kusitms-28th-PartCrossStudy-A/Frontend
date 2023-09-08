5c1d26a243c6a1f3c0c48d0ddae02702d3baed96
seungzzok <ki2684@naver.com>
2023-09-08 20:30:03 +0900

    Init: 프로젝트 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { GlobalStyle } from './global';
+import Router from './router/router';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Router />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
