384ee01eafaa698a7b430519411cbb23beae2e06
seungzzok <ki2684@naver.com>
2023-09-09 15:19:17 +0900

    Init: 초기설정 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { GlobalStyle } from './global';
-import Router from './router/router';
+// import { GlobalStyle } from './global';
+// import Router from './router/router';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <Router />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello wolrd!</h1>
     </div>
   );
 }
